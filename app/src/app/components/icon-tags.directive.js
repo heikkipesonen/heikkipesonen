77e9b2c3485c650c31c7d7bb0cef88030e4d1089
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-09-01 07:51:38 +0300

    added icon tags, modified portfolio layout

---
+++ app/src/app/components/icon-tags.directive.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  function SocialTagsController () {
+  	console.log(this.tags)
+  }
+
+
+  angular
+    .module('portfolio2')
+    .directive('iconTags', function () {
+    	return {
+    		restrict:'AE',
+    		replace:true,
+    		scope:{},
+    		bindToController:{
+    			tags:'=ngModel'
+    		},
+    		controller: SocialTagsController,
+    		controllerAs:'iconTags',
+    		template:
+    		'<div class="social-tags">'+
+	    		'<div class="tag" ng-repeat="(tagIndex, tag) in iconTags.tags track by tagIndex">'+
+	    			'<i class="ion-{{::tag}}"></i>'+
+	    		'</div>'+
+    		'</div>'
+    	};
+    });
+
+  })();
--- app/src/app/portfolio/portfolio.controller.js
+++ app/src/app/portfolio/portfolio.controller.js
@@ -9,9 +9,12 @@
   function PortfolioController ($http) {
     var vm = this;
     vm.gallery = [];
+    vm.tags = [];
 
     $http.get('assets/portfolio/portfolio.json').then(function (response) {
       vm.gallery = response.data;
+      vm.tags = _.chain(vm.gallery).pluck('tags').flatten().uniq().value();
+      console.log(vm.tags);
     });
   }
 })();
